1611a9b4ff7b3fac2d5361ee35ac1e28adab2005
MuTe43 <MuTe_wajdi@hotmail.fr>
2025-10-22 20:16:38 +0100

    add Partie 1 Workshop4(ORM/Doctrine): CRUD Author

--- src/Controller/AuthorController.php
+++ src/Controller/AuthorController.php
@@ -2,9 +2,14 @@
 
 namespace App\Controller;
 
+use App\Form\AuthorType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use App\Entity\Author;
+use App\Repository\AuthorRepository;
+use Symfony\Component\HttpFoundation\Request;
 
 final class AuthorController extends AbstractController
 {
@@ -55,4 +60,75 @@ final class AuthorController extends AbstractController
             'auth' => $auth
         ]);
     }
+
+
+
+
+    // Methode Add statique Author
+    #[Route('/addAuthor', name: 'author_statique')]
+    public function addAuthor(EntityManagerInterface $mr): Response
+    {
+        $author= new Author();
+        $author->setUsername("wajdi");
+        $author->setEmail("wajdistatique@test.com");
+        if($author){
+            $mr->persist($author);
+            $mr->flush();
+        }
+        return $this->redirectToRoute("author_list");
+    }
+
+    #[Route('/getAuthors', name:'author_getFromDB')]
+    public function getAuthors(AuthorRepository $authRepo):Response
+    {
+        $authors = $authRepo->findAll();
+        return $this->render('author/list.html.twig', [
+            'authors' => $authors,
+        ]);
+
+    }
+
+    #[Route('/insert', name: 'author_insertAuthor')]
+    public function insertAuthor(EntityManagerInterface $emi, Request $request)
+    {
+        $author = new Author();
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $emi->persist($author);
+            $emi->flush();
+            return $this->redirectToRoute("author_getFromDB");
+        }
+        return $this->render('author/form.html.twig',[
+            'authorForm' => $form,
+        ]);
+    }
+
+    #[Route('/update/{id}', name: 'author_updateAuthor')]
+    public function updateAuthor(EntityManagerInterface $emi, Request $request,$id): Response
+    {
+        $author = new Author();
+        $author = $emi->getRepository(Author::class)->find($id);
+        $form = $this->createForm(AuthorType::class, $author);
+        $form->handleRequest($request);
+        if($form->isSubmitted()){
+            $emi->persist($author);
+            $emi->flush();
+            return $this->redirectToRoute("author_getFromDB");
+        }
+
+        return $this->render('author/form.html.twig', [
+            'authorForm' => $form,
+        ]);
+    }
+
+    #[Route('/delete/{id}', name:"author_deleteAuthor")]
+    public function deleteAuthor(EntityManagerInterface $emi, $id): Response
+    {
+        $author = $emi->getRepository(Author::class)->find($id);
+        $emi->remove($author);
+        $emi->flush();
+        return $this->redirectToRoute("author_getFromDB");
+    }
+
 }
